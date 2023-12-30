1bcae3b94ecd088ef843289d09f353dac58844fe
Divyanshu Joshi <divyanshujoshi1012@gmail.com>
2023-12-30 19:06:36 +0530

    fhaekfu

--- src/components/DisplayBooks.js
+++ src/components/DisplayBooks.js
@@ -1,27 +1,52 @@
-import React from "react";
+import React, {useState} from "react";
 
 
 
 
 const DisplayBooks = ({queryResults})=>{   
+    const [selectedBook, setSelectedBook] = useState("");
     return(
-        <div style={{
-        display: "grid",
-        gridTemplateColumns: "repeat(4,1fr)",
-        }}>
-        {
-            queryResults&& (
-                queryResults.map((value, index)=>(
-                        <div key={index}>
+        <div>
+            <div style={{backgroundColor:"#71C5F461"}}>
+                {
+                    selectedBook && (
+                        <div style={{display:"flex" , color:"white"}}>
                             <img 
-                            src={value.volumeInfo.imageLinks.thumbnail} 
-                            style={{height:"200px", width:"150px" , margin:"10px"}}
-                            />                   
+                                    src={selectedBook.volumeInfo.imageLinks.thumbnail}
+                                    style={{height:"200px", width:"150px" , margin:"10px"}}
+                            />
+                            <div>
+                                <h1>{selectedBook.volumeInfo.title}</h1>
+                                <h4>{selectedBook.volumeInfo.authors[0]}</h4>
+                                <p>{selectedBook.volumeInfo.description}</p>
+                                <h5>{selectedBook.volumeInfo.}</h5>
+                                {/* words.slice(0, wordLimit).join(' '); */}
+
+                            </div>
+
                         </div>
-                ))
-            )
-        }
+                    )
+                }
+            </div>
+            <div style={{
+                display: "grid",
+                gridTemplateColumns: "repeat(5,1fr)",
+                }}>
+                {
+                    queryResults&& (
+                        queryResults.map((value, index)=>(
+                                <div key={index} onClick={()=>setSelectedBook(value)}>
+                                    <img 
+                                    src={value.volumeInfo.imageLinks.thumbnail} 
+                                    style={{height:"200px", width:"150px" , margin:"10px"}}
+                                    />                   
+                                </div>
+                        ))
+                    )
+                }
+            </div>
         </div>
+        
     )  
 } 
 
